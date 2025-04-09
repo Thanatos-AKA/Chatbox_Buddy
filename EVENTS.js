83e1cbff5577cdefba3b254c62961cb492220456
Thanatos-AKA <61469885+Thanatos-AKA@users.noreply.github.com>
2025-04-09 09:19:24 +0800

    Update EVENTS.js

--- EVENTS.js
+++ EVENTS.js
@@ -1,17 +1,17 @@
 const events = {
     "event1": {
-        "role1": "This is event1 role1 reply",
-        "role2": "This is event1 role2 reply",
-        "role3": "This is event1 role3 reply"
+        "role1": "This is event1 role1 reply from git",
+        "role2": "This is event1 role2 reply from git",
+        "role3": "This is event1 role3 reply from git"
     },
     "event2": {
-        "role1": "This is event2 role1 reply",
-        "role2": "This is event2 role2 reply",
-        "role3": "This is event2 role3 reply"
+        "role1": "This is event2 role1 reply from git",
+        "role2": "This is event2 role2 reply from git",
+        "role3": "This is event2 role3 reply from git"
     },
     "event3": {
-        "role1": "This is event3 role1 reply",
-        "role2": "This is event3 role2 reply",
-        "role3": "This is event3 role3 reply"
+        "role1": "This is event3 role1 reply from git",
+        "role2": "This is event3 role2 reply from git",
+        "role3": "This is event3 role3 reply from git"
     }
 };
