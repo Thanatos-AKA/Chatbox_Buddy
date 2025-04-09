d63f6160462d678be76e7138957c163c44034552
Thanatos-AKA <61469885+Thanatos-AKA@users.noreply.github.com>
2025-04-09 09:14:51 +0800

    Create events.js

---
+++ events.js
@@ -0,0 +1,17 @@
+const events = {
+    "event1": {
+        "role1": "This is event1 role1 reply",
+        "role2": "This is event1 role2 reply",
+        "role3": "This is event1 role3 reply"
+    },
+    "event2": {
+        "role1": "This is event2 role1 reply",
+        "role2": "This is event2 role2 reply",
+        "role3": "This is event2 role3 reply"
+    },
+    "event3": {
+        "role1": "This is event3 role1 reply",
+        "role2": "This is event3 role2 reply",
+        "role3": "This is event3 role3 reply"
+    }
+};
