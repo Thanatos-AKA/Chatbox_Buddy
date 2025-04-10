7c6784bcc854cb9b9cfec6a53f82ca79551db8ba
Thanatos-AKA <61469885+Thanatos-AKA@users.noreply.github.com>
2025-04-10 11:39:45 +0800

    Update EVENTS.js

--- EVENTS.js
+++ EVENTS.js
@@ -1,9 +1,9 @@
 const events = {
     "Shredding event": {
-        "Operator step1": "TPE Cluster SSD Shredding Requirement - 1st Nov, 2024 to Dec 31st, 2025 \n https://t.corp.amazon.com/P163509837/overview \n DAP: https://approvals.amazon.com/Approval/Details/32582057?ref_=pe_40497452_484050462 \n Red Media Moves Within of a Fence Line :https://approvals.amazon.com/Approval/Details/32869290 \n SSD tracking  ticket:" 
+        "Operator step1": "TPE Cluster SSD Shredding Requirement - 1st Nov, 2024 to Dec 31st, 2025 \n https://t.corp.amazon.com/P163509837/overview \n DAP: https://approvals.amazon.com/Approval/Details/32582057?ref_=pe_40497452_484050462 \n Red Media Moves Within of a Fence Line :https://approvals.amazon.com/Approval/Details/32869290 \n SSD tracking  ticket:",
         "Operator step2": "xx",
-        "Operator step3": "This is event1 role3 reply from git"
-        "Operator step4": "This is event1 role3 reply from git"
+        "Operator step3": "This is event1 role3 reply from git",
+        "Operator step4": "This is event1 role3 reply from git",
         "Operator step5": "This is event1 role3 reply from git"
     },
     "Master tt": {
