b6d378b47763d7f51df134e12afed4bf3fe83b6a
Thanatos-AKA <61469885+Thanatos-AKA@users.noreply.github.com>
2025-04-10 14:00:02 +0800

    Update EVENTS.js

--- EVENTS.js
+++ EVENTS.js
@@ -1,15 +1,13 @@
 const events = {
-    "Shredding event": {
-        "Operator step1": "TPE Cluster SSD Shredding Requirement - 1st Nov, 2024 to Dec 31st, 2025 \n https://t.corp.amazon.com/P163509837/overview \n DAP: https://approvals.amazon.com/Approval/Details/32582057?ref_=pe_40497452_484050462 \n Red Media Moves Within of a Fence Line :https://approvals.amazon.com/Approval/Details/32869290 \n SSD tracking  ticket:",
-        "Operator step2": "xx",
-        "Operator step3": "This is event1 role3 reply from git",
-        "Operator step4": "This is event1 role3 reply from git",
-        "Operator step5": "This is event1 role3 reply from git"
+    "Shredding tt": {
+        "step1": "TPE Cluster SSD Shredding Requirement - 1st Nov, 2024 to Dec 31st, 2025 https://t.corp.amazon.com/P163509837/overview \n DAP: https://approvals.amazon.com/Approval/Details/32582057?ref_=pe_40497452_484050462 \n Red Media Moves Within of a Fence Line :https://approvals.amazon.com/Approval/Details/32869290 \n [tracking tt title][link]",
+        "step2": "This is event2 role2 reply from git",
+        "step3": "This is event2 role3 reply from git"
     },
     "Master tt": {
-        "role1": "This is event2 role1 reply from git",
-        "role2": "This is event2 role2 reply from git",
-        "role3": "This is event2 role3 reply from git"
+        "step1": "TPE62 SSD-SHREDDING-Phiston on [day]-[month] 2025: [link]",
+        "step2": "This is event2 role2 reply from git",
+        "step3": "This is event2 role3 reply from git"
     },
     "event3": {
         "role1": "This is event3 role1 reply from git",
